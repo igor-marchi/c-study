851f64486dab06e1b45078f0a7efc3c29edbd940
Igor Marchi <igoraugustomarchi@gmail.com>
2025-09-10 21:50:56 -0300

    feat: refactor

--- bubbleSort/bubbleSort.c
+++ bubbleSort/bubbleSort.c
@@ -2,27 +2,25 @@
 
 int main()
 {
-  int arr[10] = {64, 34, 25, 12, 22, 11, 90, 88, 76, 45};
-
+  int arr[] = {64, 34, 25, 12, 22, 11, 90, 88, 76, 45};
+  int size = sizeof(arr) / sizeof(arr[0]);
   printf("Original array: \n");
-  printArray(arr);
+  printArray(arr, size);
 
-  bubbleSort(arr);
+  bubbleSort(arr, size);
   printf("Sorted array: \n");
-  printArray(arr);
+  printArray(arr, size);
 }
 
-void printArray(int arr[])
+void printArray(int arr[], int size)
 {
-  int size = sizeof(arr);
   for (int i = 0; i < size; i++)
     printf("%d ", arr[i]);
   printf("\n");
 }
 
-void bubbleSort(int arr[])
+void bubbleSort(int arr[], int size)
 {
-  int size = sizeof(arr);
   for (int i = 1; i < size; i++)
   {
     int swapped = 0;
