f30bbc6ee5ade60d9219dbd81e50f95a35c1a29d
Igor Marchi <igoraugustomarchi@gmail.com>
2025-09-11 22:11:06 -0300

    feat: recursive

---
+++ recursive/recursive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fatorial(int n)
+{
+  if (n <= 1)
+    return 1;
+
+  return n * fatorial(n - 1);
+}
+
+int main()
+{
+  int num = 5;
+  int result = fatorial(num);
+  printf("Fatorial de %d é %d\n", num, result);
+  return 0;
+}
